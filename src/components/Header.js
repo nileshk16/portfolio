4bee62ab5e73bf4302952ee2e51b1f8c9f3b0ae1
nileshk16 <nileshkumark162@gmail.com>
2024-08-07 19:43:58 +0530

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ export default function Header() {
         </nav>
         {toggleMenu && <nav className="block md:hidden ">
             <ul onClick={() => setToggleMenu(!toggleMenu)} className="flex flex-col text-white  mobile-nav">
-                <li><a href="#">Home</a></li>
+                <li><a href="/">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
